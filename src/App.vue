1f6c325ed7088fa4198ff814522ac2249bd7425b
satriyawicaksana <satriyawicaksana08@gmail.com>
2020-03-04 16:30:17 +0700

    adding navigation item component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,25 @@
     <InputBox v-model="formObj.dropshipperName" :disabled="formObj.checked" />
     <input type="text" v-model="formObj.dropshipperName" :disabled="!formObj.checked" />-->
     <div class="container">
-      <div class="stepper"></div>
+      <div class="stepper">
+        <NavigationItem
+          :id="navigationStatus[0].id+1"
+          :title="navigationStatus[0].title"
+          :active="navigationStatus[0].status"
+        />
+        <div class="arrow"></div>
+        <NavigationItem
+          :id="navigationStatus[1].id+1"
+          :title="navigationStatus[1].title"
+          :active="navigationStatus[1].status"
+        />
+        <div class="arrow"></div>
+        <NavigationItem
+          :id="navigationStatus[2].id+1"
+          :title="navigationStatus[2].title"
+          :active="navigationStatus[2].status"
+        />
+      </div>
       <div class="form"></div>
     </div>
   </div>
@@ -12,13 +30,21 @@
 
 <script>
 //import InputBox from "./components/InputBox";
+import NavigationItem from "./components/NavigationItem";
 export default {
   name: "App",
   components: {
     //InputBox,
+    NavigationItem
   },
   data: function() {
     return {
+      activeStep: 0,
+      navigationStatus: [
+        { id: 0, title: "Delivery", status: true },
+        { id: 1, title: "Payment", status: false },
+        { id: 2, title: "Finish", status: false }
+      ],
       formObj: {
         checked: true,
         dropshipperName: "",
@@ -35,5 +61,49 @@ export default {
 };
 </script>
 
-<style>
+<style lang="stylus">
+orange = #FF8A00;
+orange-stepper = #FFE4B8;
+ivory = #FFF9E4;
+dark-green = #1BD97B;
+light-green = #E8FBF1;
+grey-accent = #eeeeee;
+grey-border = #cccccc;
+black = #000;
+
+@font-face {
+  font-family: inter-medium;
+  src: url('./assets/Inter-Medium.otf');
+}
+
+*, *:before, *:after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: inter-medium;
+  font-size: 62.5%;
+}
+
+.stepper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 30%;
+  height: 50px;
+  background-color: ivory;
+  border-radius: 25px;
+}
+
+.arrow {
+  width: 8px;
+  height: 8px;
+  border: 2px solid orange;
+  border-top: none;
+  border-left: none;
+  transform: rotate(-45deg);
+}
 </style>
--- src/components/NavigationItem.vue
+++ src/components/NavigationItem.vue
@@ -13,9 +13,31 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+orange = #FF8A00;
+orange-stepper = #FFE4B8;
+
+.navigation__item {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  color: orange;
+  font-size: 1.2rem;
+}
+
 .navigation__item_id {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 0.5rem;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-color: orange-stepper;
 }
 
 .is-active {
+  color: white;
+  background-color: orange;
 }
 </style>
